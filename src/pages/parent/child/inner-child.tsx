99181b12b718b2ed350a2feb7f242eabca8602b5
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2024-10-22 13:25:36 +0300

    feat: versioning

---
+++ src/pages/parent/child/inner-child.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+// route is /parent/child/inner-child
+export default function MyReactPage() {
+  return <Layout>inner child page</Layout>;
+}
