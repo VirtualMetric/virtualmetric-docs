c9c5f51d171ef6a72d4db0ac76a8406bc491b048
Koray Erkan <koray.erkan@yahoo.com>
2024-11-17 19:46:17 +0300

    Fixed the sidebar build bug

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
 			items: [
 				{
 					type: 'docSidebar',
-					sidebarId: 'documentation',
+					sidebarId: 'documentationSidebar',
 					position: 'left',
 					label: 'Documentation',
 				},
