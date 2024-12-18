602efd9cf27b74e574d0bd42b2ac11bd9c88305b
Koray Erkan <koray.erkan@yahoo.com>
2024-12-18 09:49:45 +0300

    Removed the Highlight component

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,32 +1,17 @@
 import MDXComponents from "@theme-original/MDXComponents";
 
-import Card from "@site/src/components/Card";
-import CardBody from "@site/src/components/Card/CardBody";
-import CardFooter from "@site/src/components/Card/CardFooter";
-import CardHeader from "@site/src/components/Card/CardHeader";
-import CardImage from "@site/src/components/Card/CardImage";
-
 import {ExampleGrid, CommentCol, CodeCol} from "@site/src/components/CustomFeatures/ExampleGrid";
 
 import ImageFrame from "@site/src/components/CustomFeatures/ImageFrame";
-import Highlight from "@site/src/components/CustomFeatures/Highlight";
 import FooterCopyright from "./Footer/Copyright";
 
 export default {
 	...MDXComponents,
 
-	Card,
-	CardHeader,
-	CardBody,
-	CardFooter,
-	CardImage,
-
 	ExampleGrid,
 	CommentCol,
 	CodeCol,
 
-	Highlight,
 	ImageFrame,
-
 	FooterCopyright,
 };
