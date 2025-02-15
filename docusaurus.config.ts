b53af2c84abb1a563fad11e4670aaa4f263ab337
Yusuf Ozturk <yusuf.ozturk@outlook.com>
2025-02-15 11:30:30 +0100

    Several updates

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,27 +6,20 @@ const config: Config = {
   title: "VirtualMetric",
   tagline: "Transform data overload into actionable security insights",
   favicon: "img/favicon/favicon.ico",
-
   url: "https://virtualmetric-docs.pages.dev",
   baseUrl: "/",
-
   organizationName: "VirtualMetric",
   projectName: "virtualmetric-docs",
-
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
-
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
   },
-
   markdown: {
     mermaid: true,
   },
-
   themes: ["@docusaurus/theme-mermaid"],
-
   stylesheets: [
     {
       href: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css",
@@ -40,7 +33,13 @@ const config: Config = {
       type: "text/css"
     }
   ],
-
+  scripts: [
+    {
+      src:
+        '/js/clarity.js',
+      async: false,
+    },
+  ],
   presets: [
     [
       "classic",
@@ -49,58 +48,46 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           routeBasePath: "/",
         },
-
         googleTagManager: {
           containerId: 'VirtualMetric-10.0.0',
         },
-
         blog: {
           showReadingTime: true,
-
           feedOptions: {
             type: ["rss", "atom"],
             xslt: true,
           },
-
           onInlineTags: "warn",
           onInlineAuthors: "warn",
           onUntruncatedBlogPosts: "warn",
         },
-
         theme: {
           customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
-
   plugins: [require.resolve("docusaurus-lunr-search")],
-
   themeConfig: {
     image: "telemetry-color.jpg",
-
     colorMode: {
       defaultMode: 'light',
       disableSwitch: true,
       respectPrefersColorScheme: false,
     },
-
     docs: {
       sidebar: {
         hideable: false,
         autoCollapseCategories: false,
       },
     },
-
     mermaid: {
       theme: {
         light: 'neutral', dark: 'dark'
       }
     },
-
     navbar: {
       title: "",
-
       logo: {
         alt: "VirtualMetric Logo",
         src: "img/logo.svg",
@@ -109,7 +96,6 @@ const config: Config = {
         width: 161,
         height: 18,
       },
-
       items: [
         {
           type: "docSidebar",
@@ -117,115 +103,26 @@ const config: Config = {
           position: "left",
           label: "Documentation",
         },
-
         {
           to: "https://community.virtualmetric.com/",
           position: "left",
           label: "Community",
         },
-
         {
           to: "https://support.virtualmetric.com/",
           position: "left",
           label: "Support",
         },
-
-        // {
-        //   type: "docSidebar",
-        //   sidebarId: "tutorDocs",
-        //   position: "left",
-        //   label: "Tutorials",
-        // },
-
-        // {
-        //   href: "https://google.com",
-        //   position: "left",
-        //   label: "Community",
-        // },
-
-        // {
-        // 	type: 'docSidebar',
-        // 	sidebarId: 'apiDocs',
-        // 	position: 'left',
-        // 	label: 'API',
-        // },
-
-        // {
-        // 	to: '/blog',
-        // 	position: 'left',
-        // 	label: 'Blog'
-        // },
-
         {
           type: "search",
           position: "right",
         },
-
-        // {
-        // 	type: "dropdown",
-        // 	position: "right",
-        // 	label: "Websites",
-        // 	items: [
-        // 		{
-        // 			href: 'https://virtualmetric.com/',
-        // 			label: 'VirtualMetric Home',
-        // 		},
-        // 	],
-        // },
-
-        // {
-        // 	type: "docsVersionDropdown",
-        // 	position: "right",
-        // 	dropdownActiveClassDisabled: true,
-        // },
       ],
     },
-
     footer: {
-      // logo: {
-      //   alt: "VirtualMetric Logo",
-      //   src: "img/VirtualMetric-Logo-Footer.svg",
-      //   srcDark: "img/VirtualMetric-Logo-Footer.svg",
-      // },
-      // links: [
-      //   {
-      //     title: "Training",
-      //     items: [
-      //       {
-      //         label: "Tutorials",
-      //         to: "tutorials/intro"
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: "Development",
-      //     items: [
-      //       {
-      //         label: "API",
-      //         to: "api"
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: "Useful Links",
-      //     items: [
-      //       {
-      //         label: "VirtualMetric Home",
-      //         href: "https://virtualmetric.com/"
-      //       },
-      //       {
-      //         label: "Blog",
-      //         to: "blog"
-      //       },
-      //     ],
-      //   },
-      // ],
       copyright: `Copyright ©${new Date().getFullYear()} VirtualMetric B.V.`,
     },
-
     prism: {
-      // theme: prismThemes.github,
-      // darkTheme: prismThemes.dracula,
       theme: prismThemes.palenight,
       darkTheme: prismThemes.palenight
     },
