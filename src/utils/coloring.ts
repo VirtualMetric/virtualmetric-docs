de80bcf575ee8f8ec1034076a7d15691a59704f3
Koray Erkan <koray@virtualmetric.com>
2025-05-16 16:20:42 +0300

    Syntax coloring customization

--- src/utils/coloring.ts
+++ src/utils/coloring.ts
@@ -2,10 +2,7 @@ import {themes, type PrismTheme} from 'prism-react-renderer';
 
 const baseTheme = themes.vsDark;
 const customTheme: PrismTheme = {
-  plain: {
-    color: '#99F5F9',
-    backgroundColor: '#3D404D',
-  },
+  ...baseTheme,
   styles: [
     ...baseTheme.styles,
     {
@@ -16,67 +13,64 @@ const customTheme: PrismTheme = {
       },
     },
     {
-      types: ['parameter'],
+      // YAML keys
+      types: ['parameter', 'property'],
       style: {
         color: '#99F5F9',
       },
     },
     {
-      types: ['variable'],
-      style: {
-        color: '#99F5F9',
-      },
-    },
-    {
-      types: ['boolean', 'color', 'number', 'constant', 'property', 'rule'],
+      // Field identifiers like some_name, some_identifier
+      types: ['variable', 'attr-name'],
       style: {
         color: '#FFDE59',
       },
     },
     {
+      // Numbers and booleans
+      types: ['boolean', 'number', 'constant'],
+      style: {
+        color: '#FF0000',
+      },
+    },
+    {
+      // Other types to keep blue
       types: ['atrule', 'tag', 'script'],
       style: {
         color: '#99F5F9',
       },
     },
     {
-      types: ['operator', 'unit'],
+      // Other types to keep blue
+      types: ['operator', 'unit', 'rule'],
       style: {
         color: '#99F5F9',
       },
     },
     {
+      // Strings and multi-line content
       types: ['front-matter', 'string', 'attr-value'],
       style: {
         color: '#CAFDB5',
       },
     },
     {
+      // Other types to keep blue
       types: ['class-name'],
       style: {
         color: '#99F5F9',
       },
     },
     {
-      types: ['attr-name'],
+      // Identifiers
+      types: ['function', 'selector'],
       style: {
-        color: '#99F5F9',
+        color: '#FFDE59',
       },
     },
     {
+      // The "component_type" token
       types: ['keyword'],
-      style: {
-        color: '#99F5F9',
-      },
-    },
-    {
-      types: ['function'],
-      style: {
-        color: '#99F5F9',
-      },
-    },
-    {
-      types: ['selector'],
       style: {
         color: '#99F5F9',
       },
@@ -85,7 +79,7 @@ const customTheme: PrismTheme = {
       types: ['comment'],
       style: {
         color: '#CECECE',
-        fontStyle: 'normal',
+        fontStyle: 'normal'
       },
     },
   ],
